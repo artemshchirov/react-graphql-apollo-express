2d2ba97421e543b63b19e910f9211d58a2e24f8f
Artem Shchirov <artemshchirov@gmail.com>
2022-12-25 21:00:40 +0200

    feat(client): graphql apollo query mutations

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,91 @@
-import './App.css'
+import { useState, useEffect } from 'react';
+import { useQuery, useMutation } from '@apollo/client';
+import { GET_ALL_USERS } from './query/user';
+import { GET_ONE_USER } from './query/user';
+import { CREATE_USER } from './mutations/user';
 
-const App = () => {
-  return (
-    <div className="App">
-      Work
-    </div>
-  )
+import './App.css';
+
+interface Users {
+  id: number;
+  username: string;
+  age: number;
 }
 
-export default App
+const App = () => {
+  const { data, loading, error, refetch } = useQuery(GET_ALL_USERS, {
+    pollInterval: 500,
+  });
+  const { data: oneUser, loading: loadingOneUser } = useQuery(GET_ONE_USER, {
+    variables: {
+      id: 1,
+    },
+  });
+  const [newUser] = useMutation(CREATE_USER);
+
+  const [users, setUsers] = useState<Users[]>([]);
+  const [username, setUsername] = useState('');
+  const [age, setAge] = useState(0);
+
+  console.log(oneUser);
+
+  useEffect(() => {
+    if (!loading) setUsers(data.getAllUsers);
+  }, [data]);
+
+  const addUser = async (evt: React.SyntheticEvent) => {
+    evt.preventDefault();
+    const createdUser = await newUser({
+      variables: {
+        input: {
+          username,
+          age,
+        },
+      },
+    });
+    console.log(createdUser);
+    setUsername('');
+    setAge(0);
+  };
+
+  const getAll = (evt: React.SyntheticEvent) => {
+    evt.preventDefault();
+    refetch();
+  };
+
+  if (loading) return <h1>Loading...</h1>;
+
+  return (
+    <div className="App">
+      <form>
+        <label htmlFor="name">Name</label>
+        <input
+          value={username}
+          onChange={(evt) => setUsername(evt.target.value)}
+          id="name"
+          type="text"
+        />
+        <label htmlFor="age">Age</label>
+        <input
+          value={age}
+          onChange={(evt) => setAge(Number(evt.target.value))}
+          id="age"
+          type="number"
+        />
+        <div className="btns">
+          <button onClick={(evt) => addUser(evt)}>Create</button>
+          <button onClick={(evt) => getAll(evt)}>Receive</button>
+        </div>
+      </form>
+      <div>
+        {users.map((user) => (
+          <div className="user" key={user.id}>
+            {user.id}. {user.username} {user.age}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default App;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:5000/graphql',
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
-)
+  </ApolloProvider>,
+);
